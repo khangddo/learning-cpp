4fcb39537cf8037af6278f2f825e440a30c515f1
Ken Do <129320171+khangddo@users.noreply.github.com>
2025-12-27 02:19:43 +0000

    defined macros

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -4,8 +4,19 @@
 
 #include <iostream>
 
+#define CAPACITY 5000
+#define DEBUG 300
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+#ifdef DEBUG
+    std::cout << "DEBUG is defined!" << std::endl;
+#endif
+
+    large += small;
+    std::cout << "The large integer is: " << large << std::endl;
+
+
     std::cout << std::endl << std::endl;
     return (0);
 }
