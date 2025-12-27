a6cad29fe12464cf73ad6301608a8ea2469ad891
Ken Do <129320171+khangddo@users.noreply.github.com>
2025-12-27 02:10:02 +0000

    printed data types

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -6,6 +6,21 @@
 #include <typeinfo>
 
 int main(){
+    auto a = 8;
+    auto b = 12345678901;
+    auto c = 3.14f;
+    auto d = 3.14;
+    auto e = true;
+    auto f = 'd';
+    auto g = "C++ rocks!";
+
+    std::cout << typeid(a).name() << std::endl;
+    std::cout << typeid(b).name() << std::endl;
+    std::cout << typeid(c).name() << std::endl;
+    std::cout << typeid(d).name() << std::endl;
+    std::cout << typeid(e).name() << std::endl;
+    std::cout << typeid(f).name() << std::endl;
+    std::cout << typeid(g).name() << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
