ae97c957f12bb05d78cdf98a9509e3dc89c1da9e
Ken Do <129320171+khangddo@users.noreply.github.com>
2025-12-29 00:00:33 +0000

    Printed avergage

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -6,10 +6,19 @@
 
 int main(){
     int nums[5] = {1,23,32,24,337};
-    float result; 
+    float result = 0; 
 
     // Write your code here
-    
+    int length = sizeof(nums) / sizeof(nums[0]);
+    for (int i = 0; i < length; i++) {
+        std::cout << i << ": " << nums[i] << " of size " << sizeof(nums[i]) << std::endl;
+        result = result + (float) nums[i];
+    }
+
+    std::cout << "The total is " << result << std::endl;
+
+    result =  result / length;
+
     std::cout << "The average is " << result << std::endl;
     
     std::cout << std::endl << std::endl;
